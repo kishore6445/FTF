5fb410167239f4a2bffc61629526769cb1fd8721
ArkRavi <gitproject@arkmedis.com>
2025-04-09 20:46:20 +0530

    updated getGolas method

--- lib/data.ts
+++ lib/data.ts
@@ -250,7 +250,9 @@ export async function getRoles() {
 
 export async function getGoals(): Promise<Goal[]> {
   try {
-    const client = getClientSupabase()
+    //const client = getClientSupabase()
+    const client = createServerComponentClient({ cookies });
+
     const { data: session } = await client.auth.getSession()
 
     if (!session?.session?.user) {
