a8076e43033a523738f93e3a271c8120d2fd0832
ArkRavi <gitproject@arkmedis.com>
2025-04-15 01:23:39 +0530

    Adding force-dynamic in landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import LandingPage from "@/components/landing-page"
 
 export default function Home() {
--- components/landing-page.tsx
+++ components/landing-page.tsx
@@ -1,4 +1,5 @@
 "use client"
+export const dynamic = "force-dynamic";
 
 import Link from "next/link"
 import { Button } from "@/components/ui/button"
