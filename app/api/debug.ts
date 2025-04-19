c8992aee5e2af21ab8de00a0c55109de9794c1dd
ArkRavi <gitproject@arkmedis.com>
2025-04-19 12:09:40 +0530

    Added ruote.js in debug folder

--- app/api/debug.ts
+++
@@ -1,8 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({
-    SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-  });
-}
